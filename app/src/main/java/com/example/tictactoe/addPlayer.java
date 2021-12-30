05bdf5890e3197aca63d8196ec570b617868375d
rishabhdubeyy <rishabhdubey.28th@gmail.com>
2021-12-30 12:24:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/tictactoe/addPlayer.java
@@ -0,0 +1,44 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class addPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player);
+
+        final EditText playerOne = findViewById(R.id.playerOneName);
+        final EditText playerTwo = findViewById(R.id.playerTwoName);
+        final Button startSameBtn = findViewById(R.id.startGameBtn);
+
+
+        startSameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String getPlayerOneName = playerOne.getText().toString();
+                final String getPlayerTwoName = playerTwo.getText().toString();
+
+                if(getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()){
+                    Toast.makeText( addPlayer.this, "please enter player names", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    Intent intent = new Intent(addPlayer.this, MainActivity.class);
+                    intent.putExtra( "playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
